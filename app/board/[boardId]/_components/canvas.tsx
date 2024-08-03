d03d004a4a31bd15ccfb08736d1381f057ad7cd4
LuisSante <lsantet@unsa.edu.pe>
2024-08-02 22:09:38 -0500

    feat: 🎸 add cursor behavior

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,12 +1,14 @@
 "use client";
 
-import { useState } from "react";
+import React, { useCallback, useState } from "react";
 // import { useSelf } from "@/liveblocks.config";
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
-import { CanvasMode, CanvasState } from "@/types/canvas";
-import { useCanRedo, useCanUndo, useHistory } from "@/liveblocks.config";
+import { Camera, CanvasMode, CanvasState } from "@/types/canvas";
+import { useCanRedo, useCanUndo, useHistory, useMutation } from "@/liveblocks.config";
+import { CursorsPresence } from "./cursors-presence";
+import { pointerEventToCanvasPoint } from "@/lib/utils";
 
 interface CanvasProps {
     boardId: string;
@@ -19,9 +21,30 @@ export const Canvas = ({ boardId }: CanvasProps) => {
         mode: CanvasMode.None
     });
 
+    const [camera, setCamera] = useState<Camera>({ x: 0, y: 0 });
+
     const history = useHistory();
     const canUndo = useCanUndo();
     const canRedo = useCanRedo();
+
+    const onWheel = useCallback((e: React.WheelEvent) => {
+        setCamera((camera) => ({
+            x: camera.x - e.deltaX,
+            y: camera.y - e.deltaY
+        }));
+    }, []);
+
+    const onPointerMove = useMutation(({ setMyPresence }, e: React.PointerEvent) => {
+        e.preventDefault();
+
+        const current = pointerEventToCanvasPoint(e, camera);
+
+        setMyPresence({ cursor: current });
+    }, []);
+
+    const onPointerLeave = useMutation(({ setMyPresence }) => {
+        setMyPresence({ cursor: null });
+    }, []);
 
     return (
         <main
@@ -32,11 +55,21 @@ export const Canvas = ({ boardId }: CanvasProps) => {
             <Toolbar
                 canvasState={canvasState}
                 setCanvasState={setCanvasState}
-                canRedo={false}
-                canUndo={false}
+                canRedo={canRedo}
+                canUndo={canUndo}
                 undo={history.undo}
                 redo={history.redo}
             />
+            <svg
+                className="w-[100vh] h-[100vh]"
+                onWheel={onWheel}
+                onPointerMove={onPointerMove}
+                onPointerLeave={onPointerLeave}
+            >
+                <g>
+                    <CursorsPresence />
+                </g>
+            </svg>
         </main>
     )
 }
---
+++ app/board/[boardId]/_components/cursor.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { connectionIdToColor } from "@/lib/utils";
+import { useOther } from "@/liveblocks.config";
+import { MousePointer2 } from "lucide-react";
+import { memo } from "react";
+
+interface CursorProps {
+    connectionId: number;
+}
+
+export const Cursor = memo(({
+    connectionId
+}: CursorProps) => {
+
+    const info = useOther(connectionId, (user) => user?.info)
+    const cursor = useOther(connectionId, (user) => user.presence.cursor)
+
+    const name = info?.name || "Teammeate";
+
+    if (!cursor) {
+        return null;
+    }
+
+    const { x, y } = cursor;
+
+    console.log("se mueve en x:", x);
+    console.log("se mueve en y:", y);
+
+    return (
+        <foreignObject
+            style={{
+                transform: `translateX(${x}px) translateY(${y}px)`
+            }}
+            height={50}
+            width={name.length * 10 + 24}
+            className="relative drop-shadow-md"
+        >
+            <MousePointer2
+                className="w-5 h-5"
+                style={{
+                    fill: connectionIdToColor(connectionId),
+                    color: connectionIdToColor(connectionId),
+                }}
+            />
+            <div
+                className="left-5 absolute px-1.5 py-0.5 rounded-md font-semibold text-white text-xs"
+                style={{
+                    backgroundColor: connectionIdToColor(connectionId),
+                }}
+            >
+                {name}
+            </div>
+        </foreignObject>
+    );
+});
+
+Cursor.displayName = "Cursor";
